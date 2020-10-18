7fe4ec7d8b69c6eafdc789a2c01286b8add08064
CoreFloDev <CoreFloDev@users.noreply.github.com>
2020-10-18 21:51:53 +0100

    small refactor

--- app/src/main/java/com/example/flow_arch/MainActivity.kt
+++ app/src/main/java/com/example/flow_arch/MainActivity.kt
@@ -1,38 +1,39 @@
 package com.example.flow_arch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope: CoroutineScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            findViewById<Button>(R.id.action_btn).clicks()
-                .flowOn(Dispatchers.Default)
-                .scan(0) { prev, _ ->
-                    prev + 1
-                }
-                .collect {
-                    findViewById<TextView>(R.id.action_tv).text = it.toString()
-                }
-        }
+        findViewById<Button>(R.id.action_btn).clicks()
+            .scan(0) { prev, _ -> prev + 1 }
+            .flowOn(Dispatchers.Default)
+            .onEach { findViewById<TextView>(R.id.action_tv).text = it.toString() }
+            .launchIn(scope)
+    }
+
+    override fun onDestroy() {
+        scope.cancel()
+        super.onDestroy()
     }
 
     fun View.clicks(): Flow<Unit> = callbackFlow {
-        this@clicks.setOnClickListener {
-            this.offer(Unit)
-        }
-        awaitClose { this@clicks.setOnClickListener(null) }
+        setOnClickListener { offer(Unit) }
+        awaitClose { setOnClickListener(null) }
     }
 }
