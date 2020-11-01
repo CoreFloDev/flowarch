0ae51c648b0443fbe8a2e4be6cedf133f11c6d8a
CoreFloDev <CoreFloDev@users.noreply.github.com>
2020-11-01 17:22:55 +0000

    swap okhttp with ktor and moshi with kotlin serialisation

--- app/src/main/java/com/example/flow_arch/common/di/AppModule.kt
+++ app/src/main/java/com/example/flow_arch/common/di/AppModule.kt
@@ -2,13 +2,15 @@ package com.example.flow_arch.common.di
 
 import android.content.Context
 import com.example.flow_arch.common.network.MovieApi
+import com.example.flow_arch.common.network.MovieApiImplementation
 import com.example.flow_arch.common.repo.MovieRepository
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.http.*
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 
 @Module
 class AppModule(private val context: Context) {
@@ -19,24 +21,15 @@ class AppModule(private val context: Context) {
 
     @Provides
     @AppScope
-    fun provideRetrofit(): Retrofit =
-        Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/")
-            .addConverterFactory(json.asConverterFactory("application/json;charset=utf-8".toMediaType()))
-            .build()
-//    @Provides
-//    @AppScope
-//    fun provideHttpClient() = HttpClient {
-//        HttpClient(OkHttp) {
-//            // Json
-//            install(JsonFeature) {
-//                serializer = KotlinxSerializer(json)
-//                accept(ContentType.Application.Json)
-//                charset("utf-8")
-//            }
-//
-//        }
-//    }
+    fun provideHttpClient() = HttpClient {
+        // Json
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(json)
+            accept(ContentType.Application.Json)
+            charset("utf-8")
+        }
+
+    }
 
     private val json = Json {
         ignoreUnknownKeys = true
@@ -44,13 +37,9 @@ class AppModule(private val context: Context) {
         encodeDefaults = false
     }
 
-//    @Provides
-//    @AppScope
-//    fun provideMovieApi(httpClient: HttpClient): MovieApi = MovieApiImplementation(httpClient)
-
-        @Provides
+    @Provides
     @AppScope
-    fun provideMovieApi(retrofit: Retrofit): MovieApi = retrofit.create(MovieApi::class.java)
+    fun provideMovieApi(httpClient: HttpClient): MovieApi = MovieApiImplementation(httpClient)
 
     @Provides
     @AppScope
--- app/src/main/java/com/example/flow_arch/common/network/MovieApi.kt
+++ app/src/main/java/com/example/flow_arch/common/network/MovieApi.kt
@@ -1,22 +1,21 @@
 package com.example.flow_arch.common.network
 
+import io.ktor.client.*
+import io.ktor.client.request.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import retrofit2.http.GET
 
 interface MovieApi {
-
-    @GET("3/movie/top_rated?api_key=06c477fb6235927e8e8ea7e96b18133c")
     suspend fun getMovieList(): ResultList
 }
 
-//class MovieApiImplementation(
-//    private val client: HttpClient
-//): MovieApi {
-//    override suspend fun getMovieList(): ResultList {
-//        return client.get("https://api.themoviedb.org/3/movie/top_rated?api_key=06c477fb6235927e8e8ea7e96b18133c")
-//    }
-//}
+class MovieApiImplementation(
+    private val client: HttpClient
+): MovieApi {
+    override suspend fun getMovieList(): ResultList {
+        return client.get("https://api.themoviedb.org/3/movie/top_rated?api_key=06c477fb6235927e8e8ea7e96b18133c")
+    }
+}
 
 @Serializable
 data class ResultList(
