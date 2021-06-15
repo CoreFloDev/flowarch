b0d21f1c4e276337825fd759dd0471384bebd1df
CoreFloDev <CoreFloDev@users.noreply.github.com>
2021-06-15 16:39:25 +0200

    add use cases unit tests

---
+++ app/src/test/java/com/example/flow_arch/main/usecases/IncrementCounterUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.example.flow_arch.main.usecases
+
+import app.cash.turbine.test
+import kotlin.time.ExperimentalTime
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class IncrementCounterUseCaseTest {
+
+    private val useCase = IncrementCounterUseCase()
+
+    @ExperimentalTime
+    @Test
+    fun `nominal test case`() {
+        runBlockingTest {
+            flowOf(Action.IncrementNumber)
+                .let(useCase())
+                .test {
+                    assertEquals(Result.UiUpdate.IncrementNumber, expectItem())
+                    expectComplete()
+                }
+        }
+    }
+}
--- app/src/test/java/com/example/flow_arch/main/usecases/LoadMovieListUseCaseTest.kt
+++ app/src/test/java/com/example/flow_arch/main/usecases/LoadMovieListUseCaseTest.kt
@@ -1,15 +1,17 @@
 package com.example.flow_arch.main.usecases
 
 import app.cash.turbine.test
+import com.example.flow_arch.common.network.Movie
 import com.example.flow_arch.common.repo.MovieRepository
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
+import java.lang.Exception
+import kotlin.time.ExperimentalTime
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import kotlin.time.ExperimentalTime
 
 class LoadMovieListUseCaseTest {
 
@@ -20,53 +22,38 @@ class LoadMovieListUseCaseTest {
     @ExperimentalTime
     @Test
     fun `test nominal case`() {
-        every { movieRepo.getMovieList() } returns emptyFlow()
+        every { movieRepo.getMovieList() } returns flowOf(listOf(Movie(OVERVIEW, IMAGE, TITLE)))
 
         runBlockingTest {
             flowOf(Action.InitialAction)
                 .let(useCase())
                 .test {
-                    assertEquals(expectItem(), Result.UiUpdate.IncrementNumber)
+                    assertEquals(Result.UiUpdate.MovieList.Loading, expectItem())
+                    assertEquals(Result.UiUpdate.MovieList.Display(listOf(Movie(OVERVIEW, IMAGE, TITLE))), expectItem())
                     expectComplete()
                 }
         }
     }
 
-
+    @ExperimentalTime
     @Test
-    fun `test shared flow error`() {
+    fun `test error case`() {
+        every { movieRepo.getMovieList() } returns flow { throw Exception() }
+
         runBlockingTest {
-            var numberOfReceived = 0
-            var shareScope = CoroutineScope(Dispatchers.Default)
-
-            for (i in 1..100) {
-                var mainScope = CoroutineScope(Dispatchers.Default)
-
-                println("flow started $i")
-
-
-                val upstream = emptyFlow<Content>()
-                    .onStart { emit(Content.A) }
-                    .shareIn(shareScope, SharingStarted.Lazily)
-
-                listOf(
-                    upstream.filterIsInstance<Content.A>().onEach {
-                        println("content received $it")
-                        numberOfReceived++
-                        mainScope.cancel()
-                    },
-                    upstream.filterIsInstance<Content.B>()
-                ).merge()
-                    .launchIn(mainScope)
-
-            }
-            println(numberOfReceived)
+            flowOf(Action.InitialAction)
+                .let(useCase())
+                .test {
+                    assertEquals(Result.UiUpdate.MovieList.Loading, expectItem())
+                    assertEquals(Result.UiUpdate.MovieList.Error, expectItem())
+                    expectComplete()
+                }
         }
-
     }
 
-    sealed class Content {
-        object A : Content()
-        object B : Content()
+    companion object {
+        private const val OVERVIEW = "overview"
+        private const val IMAGE = "image"
+        private const val TITLE = "title"
     }
 }
